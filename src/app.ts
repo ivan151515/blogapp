0ccdf9aaa9e7e973cfb76bde11d69a5bb5acf5da
Ivan Batur <ivanbatur2g@gmail.com>
2024-01-01 18:29:55 +0100

    Added cors helmet and compression middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import "express-async-errors";
-
+import cors from "cors";
 import { unknownEndpoint } from './middleware/unknownEndpoint';
 import { errorHandler } from './middleware/errorHandler';
 import router from './api/routes';
+import compression from 'compression';
+import helmet from 'helmet';
 const app = express();
 app.use(express.json());
+app.use(compression());
+app.use(cors());
+app.use(helmet());
 
 app.use("/api", router);
 app.use(unknownEndpoint);
